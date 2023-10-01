5422fc63edbafe2c9c16ee7a3e7fc288016b8ab0
Trịnh Huy Nam <38585889+huynamboz@users.noreply.github.com>
2023-10-01 15:42:01 +0700

    Live chat UI (#2)
    * UI: live chat UI

---
+++ src/components/chats/ChatModules/MessageContent.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps(['message'])
+import { authStore } from '@/stores/auth.store'
+</script>
+<template>
+  <div class="flex items-end gap-3">
+    <img
+      v-if="authStore?.user?.id !== message.sender_id"
+      class="w-[35px] h-[35px] rounded-full object-cover"
+      src="https://avatars.githubusercontent.com/u/38585889?v=4"
+      alt=""
+    />
+    <div class="flex flex-col gap-1" :class="{ 'ml-auto': authStore.user.id == message.sender_id }">
+      <p v-if="authStore.user.id != message.sender_id" class="text-xs">{{ message.name }}</p>
+      <p
+        class="bg-[var(--bg-primary)] p-3 rounded-[20px] w-[90%]"
+        :class="{ 'bg-[var(--green-primary)]': authStore.user.id == message.sender_id }"
+      >
+        {{ message.content }}
+      </p>
+    </div>
+  </div>
+</template>
